dcbd168c1c5d29080a13b60679030def30f1fb37
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-11 16:35:50 -0300

    feat(style): add reset CSS w/ normalize && add Rubik font

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import Head from 'next/head';
 import theme from '../src/theme'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import { GlobalStyle } from '../src/theme/GlobalStyle'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <title>InstaDev</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
