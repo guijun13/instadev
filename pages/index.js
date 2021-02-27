b8189011fba54841e4d511977d7ce965c721645f
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-27 15:53:33 -0300

    feat(modal): create 'Modal' on homepage w/ basic functionalities
    Open and close modal when clicked on button. Close it when clicked outside and won't close when
    clicking inside the modal

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 import Text from '../src/components/foundation/Text';
 import Button from '../src/components/commons/Button';
 import Grid from '../src/components/foundation/layout/Grid';
 import Box from '../src/components/foundation/layout/Box';
+import Modal from '../src/components/commons/Modal';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <Box
       flex={1}
@@ -18,6 +21,20 @@ export default function Home() {
       backgroundRepeat="no-repeat"
       backgroundPosition="bottom right"
     >
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        {(modalProps) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <Box backgroundColor="white" {...modalProps}>
+            {/* data-modal-safe-area="true" da funcao {children()} do componente <Modal /> */}
+            <div>Modal o brabo das galaxias</div>
+          </Box>
+        )}
+      </Modal>
       <Menu />
 
       <Grid.Container
@@ -67,6 +84,9 @@ export default function Home() {
               }}
               display="block"
               variant="primary.main"
+              onClick={() => {
+                setIsModalOpen(!isModalOpen);
+              }}
             >
               Cadastrar
             </Button>
