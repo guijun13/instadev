f0da7acf1bf8f418d0c4f6415c4ec3639fc7654c
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-11 22:17:45 -0300

    feat(faq): create 'faqScreen' component to 'faq' page and add 'Box' and 'Grid' style
    'faqScreen' info dynamically fetch with nextjs 'getStaticProps' to pre-requent the api to render
    together with the rest of html

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,5 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQ() {
-  return <div>FAQ</div>;
+export default function FAQ({ faqCategories }) {
+  /* Se fosse feito a request em React puro
+  const [faqCategories, setFaqCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://instalura-api.vercel.app/api/content/faq')
+      .then((serverResponse) => serverResponse.json())
+      .then((convertedResponse) => convertedResponse.data)
+      .then((response) => {
+        setFaqCategories(response);
+      });
+  }, []);
+  */
+
+  return <FAQScreen faqCategories={faqCategories} />;
 }
+
+// Next.js já faz o pré-request da API ao utilizar o getStaticProps, assim o
+// conteúdo aparece juntamente com o resto da página
+export async function getStaticProps() {
+  const faqCategories = await fetch(
+    'https://instalura-api.vercel.app/api/content/faq'
+  )
+    .then((serverResponse) => serverResponse.json())
+    .then((convertedResponse) => convertedResponse.data);
+
+  return {
+    props: {
+      faqCategories,
+    }, // will be passed to the page component as props
+  };
+}
+
+FAQ.propTypes = {
+  faqCategories: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      slug: PropTypes.string,
+      questions: PropTypes.arrayOf(
+        PropTypes.shape({
+          title: PropTypes.string,
+          slug: PropTypes.string,
+          description: PropTypes.string,
+        })
+      ),
+    })
+  ).isRequired,
+};
