9367c00a28c894dcc5b2c206da139dc154267d66
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-24 23:13:17 -0300

    feat(profile): create blank 'Profile' app page

---
+++ pages/app/profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ProfilePage() {
+  return (
+    <>
+      Página de profile
+      <img
+        src="https://media.giphy.com/media/bn0zlGb4LOyo8/giphy.gif"
+        alt="Nicolas Cage"
+      />
+    </>
+  );
+}
