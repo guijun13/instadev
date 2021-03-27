3a2f4cfe5a7e6afc159e7803f9fd5d6a1392c2e4
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-27 18:29:29 -0300

    fix(login): fix 'fill the login fields and go to /app/profile page' test

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -4,9 +4,9 @@ describe('/pages/app/login', () => {
   // it = teste que estamos fazendo
   it('fill the login fields and go to /app/profile page', () => {
     cy.visit('/app/login/'); // va para a pag /app/login/
-    cy.get('#formCadastro input[name="usuario"]').type('guijun13'); // no elemento input, digite
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura'); // no elemento input, digite
-    cy.get('#formCadastro button[type="submit"]').click(); // no elemento botao, clique
+    cy.get('#registerForm input[name="user"]').type('guijun13'); // no elemento input, digite
+    cy.get('#registerForm input[name="password"]').type('senhasegura'); // no elemento input, digite
+    cy.get('#registerForm button[type="submit"]').click(); // no elemento botao, clique
     cy.url().should('include', '/app/profile'); // verificar a url acessada
   });
 });
