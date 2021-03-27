6499621724ff93e8635c41db841786f87f02fca5
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-27 18:26:25 -0300

    feat(login): add 'nookie' package for storaging token with cookies

--- src/components/services/login/loginService.js
+++ src/components/services/login/loginService.js
@@ -1,3 +1,5 @@
+import { destroyCookie, setCookie } from 'nookies';
+
 async function HttpClient(url, { headers, body, ...options }) {
   return fetch(url, {
     headers: {
@@ -29,7 +31,17 @@ export const loginService = {
       }
     ).then((convertedResponse) => {
       console.log(convertedResponse);
-      return convertedResponse;
+      const { token } = convertedResponse.data;
+      const DAY_IN_SECONDS = 86400;
+      // Salvar o token
+      setCookie(null, 'APP_TOKEN', token, {
+        path: '/', // definir que todas as pag terao acesso ao cookie
+        maxAge: DAY_IN_SECONDS * 7, // tempo para cookie expirar
+      });
+      return { token };
     });
+  },
+  logout() {
+    destroyCookie(null, 'APP_TOKEN');
   },
 };
