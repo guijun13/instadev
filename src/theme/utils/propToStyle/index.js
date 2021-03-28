e134b6c0875b9519d1e8441e310534fdfa165d17
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-28 17:11:33 -0300

    test(proptostyle): create tests for 'propToStyle' function and fix it
    Test every input data type (string, number or object)

---
+++ src/theme/utils/propToStyle/index.js
@@ -0,0 +1,42 @@
+// import { css } from 'styled-components';
+import breakpointsMedia from '../breakpointsMedia';
+
+export default function propToStyle(propName) {
+  return (props) => {
+    const propValue = props[propName];
+
+    if (typeof propValue === 'object') {
+      const breakpoints = {};
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.sm) breakpoints.sm = { [propName]: propValue.sm };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+      if (propValue.lg) breakpoints.lg = { [propName]: propValue.lg };
+      if (propValue.xl) breakpoints.xl = { [propName]: propValue.xl };
+
+      return breakpointsMedia(breakpoints);
+
+      // return breakpointsMedia({
+      //   xs: {
+      //     [propName]: propValue.xs,
+      //   },
+      //   sm: {
+      //     [propName]: propValue.sm,
+      //   },
+      //   md: {
+      //     [propName]: propValue.md,
+      //   },
+      //   lg: {
+      //     [propName]: propValue.lg,
+      //   },
+      //   xl: {
+      //     [propName]: propValue.xl,
+      //   },
+      // });
+    }
+
+    return {
+      // textAlign: props.textAlign
+      [propName]: props[propName],
+    };
+  };
+}
