478c19ee67d08449aa8fb74e15731d44dee82eab
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-27 22:46:02 -0300

    test(login): better describe login test

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,30 +2,32 @@
 
 import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.PageObject';
 
-describe('/pages/app/login', () => {
-  // it = teste que estamos fazendo
-  it('fill the login fields and go to /app/profile page', () => {
-    cy.intercept(
-      'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
-    ).as('userLogin'); // interceptar a url
+describe('/pages/app/login/', () => {
+  describe('when fill and submit a form login request', () => {
+    it('go to the profile page', () => {
+      /* Pré test */
+      cy.intercept(
+        'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
+      ).as('userLogin'); // interceptar a url
 
-    /* Montagem do cenário de teste */
-    const loginScreen = new LoginScreenPageObject(cy);
-    loginScreen
-      .fillLoginForm({
-        user: 'guijun13',
-        password: 'senhasegura',
-      })
-      .submitLoginForm();
-    /* ---------------------------- */
+      /* Montagem do cenário de teste */
+      const loginScreen = new LoginScreenPageObject(cy);
+      loginScreen
+        .fillLoginForm({
+          user: 'guijun13',
+          password: 'senhasegura',
+        })
+        .submitLoginForm();
 
-    cy.url().should('include', '/app/profile'); // verificar a url acessada
-    // espera a interceptação da url
-    cy.wait('@userLogin').then((intercept) => {
-      const { token } = intercept.response.body.data; // token do servidor
-      cy.getCookie('APP_TOKEN')
-        .should('exist') // verifica se existe
-        .should('have.property', 'value', token); // verifica se o token do cookie é igual ao do server
+      /* Asserções */
+      cy.url().should('include', '/app/profile'); // verificar a url acessada
+      // espera a interceptação da url
+      cy.wait('@userLogin').then((intercept) => {
+        const { token } = intercept.response.body.data; // token do servidor
+        cy.getCookie('APP_TOKEN')
+          .should('exist') // verifica se existe
+          .should('have.property', 'value', token); // verifica se o token do cookie é igual ao do server
+      });
     });
   });
 });
