14947966d2f209b2b0f0763bbe82026136fd86cc
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-31 00:39:55 -0300

    test(loginform): add 'loginForm' test for when field is invalid

--- src/components/forms/TextField/TextField.test.js
+++ src/components/forms/TextField/TextField.test.js
@@ -17,4 +17,28 @@ describe('<TextField />', () => {
     const textField = screen.getByPlaceholderText(/jun/i);
     expect(textField).toMatchSnapshot();
   });
+
+  describe('when field is valid', () => {});
+  describe('when field is invalid', () => {
+    // o que esperamos que aconteca
+    test('displays the repective error message', () => {
+      render(
+        <TextField
+          placeholder="Email"
+          value="jun@gmail.com"
+          onChange={() => {}}
+          name="email"
+          isTouchedField
+          error="O campo é obrigatorio"
+        />
+      );
+
+      const inputEmail = screen.getByPlaceholderText(/email/i);
+      expect(inputEmail).toHaveValue('jun@gmail.com');
+      expect(screen.getByRole('alert')).toHaveTextContent(
+        'O campo é obrigatorio'
+      );
+      expect(inputEmail).toMatchSnapshot();
+    });
+  });
 });
