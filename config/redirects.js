f5db187a4b8fac347845bbc8ebd40c82c1421e70
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-08 00:50:13 -0300

    refactor(about): change pt-br name 'Sobre' for 'About' on its page and files

--- config/redirects.js
+++ config/redirects.js
@@ -6,8 +6,8 @@ module.exports = [
     permanent: true,
   },
   {
-    source: '/about/',
-    destination: '/sobre',
+    source: '/sobre/',
+    destination: '/about',
     permanent: true,
   },
   {
